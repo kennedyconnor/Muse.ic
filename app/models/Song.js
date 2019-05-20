23b409c56944e611e40c2b44df06ba1d9ffbc781
Connor Kennedy <kennedyconnor02@gmail.com>
2019-05-19 21:32:37 -0600

    working project

--- app/models/Song.js
+++ app/models/Song.js
@@ -9,11 +9,35 @@ export default class Song {
         this.preview = song.previewUrl
     }
 
+    //     get Template() {
+    //         return `<li> ${this.title} --- ${this.artist}</li>
+    //          <audio onplay="app.controllers.itunesCtrl.autoPause('${this.preview}')" controls>
+    //             <source src="${this.preview}" type="audio/mpeg">
+    //             Your browser does not support the audio tag.
+    //          </audio> `
+    //     }
+    // 
+
+
     get Template() {
-        return `<li> ${this.title} --- ${this.artist}</li>
-                 <audio onplay="app.controllers.itunesCtrl.autoPause('${this.preview}')" controls>
-  <source src="${this.preview}" type="audio/mpeg">
-  Your browser does not support the audio tag.
-</audio> `
+        return `
+        <div class="col-3">
+            <div class="card" style="width: 22rem;">
+                <img src="${this.albumArt}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${this.title}</h5>
+                        <p class="card-text" style="list-style:none;">
+                       ${this.artist} <br>
+                        ${this.collection} <br>
+                        $${this.price}<br>
+                        </p>
+                         <audio onplay="app.controllers.itunesCtrl.autoPause('${this.preview}')" controls>
+                         <source src="${this.preview}" type="audio/mpeg">
+                         Your browser does not support the audio tag.
+                         </audio>
+                    </div>
+                </div>
+            </div>
+            }`
     }
 }
